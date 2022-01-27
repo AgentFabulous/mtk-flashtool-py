be3786f1dfe041765e933053451fa134177c0a4b
Kshitij Gupta <kshitijgm@gmail.com>
2022-01-27 13:33:27 +0530

    mtk-flashtool-py: Initial patchset

---
+++ chip_mapping.py
@@ -0,0 +1,10 @@
+chip_map = {
+    0x766: {
+        'id': 0x766,
+        'name': 'MT6765',
+        'hw_code': 0x6765,
+        # Following should be present in DA header ideally
+        'da_load_addr': 0x200000,
+        'da_sig_len': 0x100,
+    }
+}
--- main.py
+++ main.py
@@ -3,8 +3,12 @@ import sys
 import time
 
 import serial
+from struct import unpack
+
+from chip_mapping import chip_map
 
 SKIP_SLA = False
+DEBUG = False
 
 
 def serial_ports():
@@ -13,7 +17,8 @@ def serial_ports():
     elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
         _ports = glob.glob('/dev/tty[A-Za-z]*')
     elif sys.platform.startswith('darwin'):
-        _ports = glob.glob('/dev/tty.*')
+        # Don't accidentally look up wlan-debug and bluetooth ports
+        _ports = glob.glob('/dev/tty.usbmodem*')
     else:
         raise EnvironmentError('Unsupported platform')
 
@@ -41,63 +46,110 @@ def write_serial_raw(s, data, nowrite=False):
     sdata = []
     for cmd_byte in data:
         hex_byte = ("{0:02x}".format(cmd_byte))
-        #print(hex_byte, end=' ')
+        # print(hex_byte, end=' ')
         sdata.append(hex_byte)
         if not nowrite:
             s.write(bytearray.fromhex(hex_byte))
-    print('TX ->', hex_list_to_str(sdata))
+    if DEBUG:
+        print('TX ->', hex_list_to_str(sdata))
 
 
 def read_serial(s, byte_count):
     ret = []
     for i in range(byte_count):
         ret.append(s.read(1).hex())
-    print('RX <-', hex_list_to_str(ret))
+    if DEBUG:
+        print('RX <-', hex_list_to_str(ret))
     return ret
 
 
 def try_handshake(s, port):
-    print('\nAttempting handshake on', port)
+    print('\nAttempting handshake on [{}]...'.format(port))
     data = 'a00a5005'
-    rdata = ''
+    is_preloader = False
+    handshake_magic = '5ff5affa'
+    preloader_greet = str(b'READY'.hex())
     cmd_bytes = bytearray.fromhex(data)
-    for cmd_byte in cmd_bytes:
-        _data = ("{0:02x}".format(cmd_byte))
-        write_serial(s, _data)
-        rdata += ''.join(read_serial(s, 1))
-    if rdata == '5ff5affa':
+    retry = True
+    rdata = ''
+    while retry:
+        rdata = ''
+        for cmd_byte in cmd_bytes:
+            _data = ("{0:02x}".format(cmd_byte))
+            write_serial(s, _data)
+            while not s.in_waiting:
+                continue
+            rdata += ''.join(read_serial(s, 1))
+            rb_count = s.in_waiting
+            for i in range(rb_count):
+                rdata += ''.join(read_serial(s, 1))
+            if rdata == preloader_greet:
+                is_preloader = True
+                break
+            retry = False
+    if rdata == handshake_magic:
         print('Handshake success! Found MTK device at', port)
+        print('Device connected via', 'Preloader' if is_preloader else "BootROM")
         return True
     elif rdata == data:
-        print('Handshake complete. Warning: device returned input data.')
+        print('Handshake complete. Warning: device returned input data. (Is Handshake already done?)')
     else:
         print('Handshake failed! Aborting.')
         sys.exit(0)
 
 
 def check_preloader(s):
-    # CMD_GET_BL_VER
-    print('\nChecking preloader version')
+    print('\nChecking preloader version...')
     data = 'fe'
     write_serial(s, data)
-    if ''.join(read_serial(s, 1)) == data:
-        print('Invalid preloader version! BROM connection.')
+    rb = ''.join(read_serial(s, 1))
+    if rb == data:
+        print('Invalid preloader version! BootROM connection.')
     else:
-        print('Preloader connection. Preloader version check not implemented, aborting.')
-        sys.exit(0)
+        print('Preloader version = 0x' + rb)
 
 
-def get_soc_id(s):
-    print('\nQuerying SoC ID')
-    data = 'e7'
-    write_serial(s, data)
-    if ''.join(read_serial(s, 1)) == data:
-        read_serial(s, 4)
-        read_serial(s, 32)
-        read_serial(s, 2)
-    else:
-        print('Get SoC ID failed! Aborting')
-        sys.exit(0)
+def read_resp(s, cmd, inital_sleep=0.1):
+    write_serial(s, cmd)
+    if ''.join(read_serial(s, 1)) == cmd:
+        if DEBUG:
+            print("ACK - [", cmd, ']')
+        ctr = 0
+        time.sleep(inital_sleep)
+        while not s.in_waiting:
+            time.sleep(0.1)
+            if ctr == 15:  # Keep checking for responses for 1.5 seconds
+                break
+            else:
+                ctr += 1
+                continue
+        return read_serial(s, s.in_waiting)
+
+
+def bstr_to_int(bstr):
+    return int.from_bytes(bytes.fromhex(''.join(bstr)), "big")
+
+
+def get_chip_id(s):
+    print('\nQuerying Chip ID...')
+    hw_code_cmd = 'fd'
+    hw_sub_cmd = 'fc'
+    hw_code_resp = bstr_to_int(read_resp(s, hw_code_cmd))
+    hw_sub_resp = bstr_to_int(read_resp(s, hw_sub_cmd))
+    chip_config = chip_map[hw_code_resp >> 16]
+    chip_config['hw_sub_code'] = (hw_sub_resp >> 32) >> 16
+    chip_config['hw_version'] = (hw_sub_resp >> 32) & 0xFFFF
+    chip_config['sw_version'] = hw_sub_resp & 0xFFFFFFFF
+    print("==============================")
+    print("======= Chip detected! =======")
+    print("==============================")
+    print("======= {} ({}) =======".format(chip_config['name'], hex(chip_config['id'])))
+    print("  Hardware Code:", hex(chip_config['hw_code']))
+    print("  Hardware Sub-code:", hex(chip_config['hw_sub_code']))
+    print("  Hardware Version:", hex(chip_config['hw_version']))
+    print("  Software Version:", hex(chip_config['sw_version']))
+    print("==============================")
+    return chip_config
 
 
 def load_auth_file():
@@ -150,9 +202,23 @@ def qualify_host(s):
 
 
 def load_da():
-    f = open('MTK_AllInOne_DA_mt6765_mt6785.bin', 'rb')
-    f.seek(int("0x39DC", 0))
-    return f.read(229376)
+    target_da = 'MTK_AllInOne_DA.bin'
+    f = open(target_da, 'rb')
+    pos = 0x68
+    f.seek(pos)
+    print("\n==============================")
+    print("========= DA Loader ==========")
+    print("==============================")
+    print("  Loading {}...".format(target_da))
+    print("  Supported Chips: ")
+    da_count = unpack("<I", f.read(4))[0]
+    for i in range(da_count):
+        hw_code = hex(unpack("<I", f.read(4))[0] >> 16)
+        print('    - MT' + str(hw_code).split('x')[1])
+        # Comparing len between each "0xDADA" in header, consistently 220
+        f.seek(f.tell() + 220 - 4)
+    print("  Loaded DA!\n  Found {} supported chips.".format(da_count))
+    print("==============================")
 
 
 def send_da(s):
@@ -175,8 +241,8 @@ def send_da(s):
         read_serial(s, 2)
         ba = load_da()
         print('Loaded DA. Sending...')
-        for i in range(int(len(ba)/8192)):
-            write_serial_raw(s, ba[i*8192:(i+1)*8192])
+        for i in range(int(len(ba) / 8192)):
+            write_serial_raw(s, ba[i * 8192:(i + 1) * 8192])
         read_serial(s, 2)
         read_serial(s, 2)
 
@@ -193,8 +259,9 @@ if __name__ == '__main__':
             ser = serial.Serial(port=ports[0], baudrate=115200)
             try_handshake(ser, ports[0])
             check_preloader(ser)
-            get_soc_id(ser)
-            send_auth_file(ser)
-            qualify_host(ser)
-            send_da(ser)
+            get_chip_id(ser)
+            load_da()
+            #            send_auth_file(ser)
+            #            qualify_host(ser)
+            #            send_da(ser)
             abort = True
